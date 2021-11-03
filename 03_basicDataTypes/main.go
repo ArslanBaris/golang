d71e5e3dac7d5d9c92549140e122e291fbcbf29c
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-11-03 14:19:19 +0300

    basic data types

---
+++ 03_basicDataTypes/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name string = "Arin"
+	var name = "Arin"
+	var age int16 = -256
+	var isMarried bool = true
+	var weight float32 = 72.5
+
+	isMarried = false
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	fmt.Println(weight)
+
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", isMarried)
+	fmt.Printf("%T\n", weight)
+
+}
