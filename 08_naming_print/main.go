e6ece1f5f0282a34e4d368f04005e53ceda02dd0
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-11-04 16:20:30 +0300

    naming print

---
+++ 08_naming_print/main.go
@@ -0,0 +1,54 @@
+package main
+
+var x = 10
+
+func main() {
+
+	// (Print - Println) - Printf
+
+	/* 	fmt.Print("Merhaba")
+	   	fmt.Println("Merhaba") // + \n
+	   	fmt.Printf("Merhaba")
+	*/
+	//name := "Baris"
+	/*
+		fmt.Print(name)
+		fmt.Println(name)
+		fmt.Printf(name)
+	*/
+
+	/* fmt.Print("Name:", name) 		// Name:Baris
+	fmt.Println("")
+	fmt.Println("Name:", name) 		// Name: Baris
+	fmt.Printf("Name:", name)  		// Name:%!(EXTRA string=Baris)
+	fmt.Println("")
+	fmt.Printf("Name: %v", name)	// Name: Baris */
+
+	/* x := 100
+	y := 20
+	z := 30
+
+	fmt.Printf("%b %d %o", x, y, z) // %b -> base 2  -  %d -> base 10  -  %o -> base 8 */
+
+	//name, age := "Arin", 5
+
+	//fmt.Print("Benim Adım ", name, ", ve ben ", age, " yaşındayım.")
+	//fmt.Println("Benim Adım", name, "ve ben", age, "yaşındayım.")
+	//fmt.Printf("Benim Adım %v, ve ben %v yaşındayım", name, age)
+
+	// VISIBILITY
+
+	//fmt.Println(x)
+
+	/* var coin string  // count - customer - coin
+	// Go camel case isimlendirme kullanılır
+	var coinType string
+	var custName string
+	// kısaltmalar büyük harflerle yazılır
+	var URL  // Url değil
+	var HTTP  // http değil "xyzHTTP"
+	i , j , k  */
+
+	// benForDongusuDegiskeninİsmiyim
+
+}
