0279f13ba0ca09c63016fe5b6318ff3af40ac01e
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-11-04 20:38:18 +0300

    typeless constants

---
+++ 12_typeless_constants/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* const x = 5
+	fmt.Printf("%T, %v\n", x, x)
+	*/
+
+	/* 	const x = 3
+	   	var y int16 = 12
+
+	   	fmt.Printf("%T, %v\n", x, x)
+	   	fmt.Printf("%T, %v\n", y, y)
+
+	   	fmt.Printf("%T, %v\n", x+y, x+y) // int16(x) + y */
+
+	/* 	const x = int16(5.2 + 4.8)
+	   	fmt.Printf("%T, %v\n", x, x) */
+
+	/*
+		// const x int32 = 3
+		// const y float32 = 5.6 	// x+y toplanamaz
+		const x = 3 	// typeless const
+		const y = 5.6
+		fmt.Printf("%T, %v\n", x, x)
+		fmt.Printf("%T, %v\n", y, y)
+		fmt.Printf("%T, %v\n", x+y, x+y) */
+
+	const x = 3
+	const y = 5.6
+	const z = true
+	const t = "test"
+
+	fmt.Printf("%T, %v\n", x, x)
+	fmt.Printf("%T, %v\n", y, y)
+	fmt.Printf("%T, %v\n", z, z)
+	fmt.Printf("%T, %v\n", t, t)
+
+}
