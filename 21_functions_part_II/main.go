12cfbeebf4fe4dd5b09867665e843f73a983fd51
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-11-08 13:55:56 +0300

    functions part-II

---
+++ 21_functions_part_II/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+
+	merhaba("Baris", 21) // Argument Function Run
+
+	fmt.Println(result(40))
+
+	var x int = 10
+	fmt.Println(x)
+
+	var moment time.Time = time.Now() // Now ---> method
+	fmt.Println(moment)
+
+	//
+
+	fmt.Print("Lütfen Sınav Sonucunuzu Giriniz: ")
+	reader := bufio.NewReader(os.Stdin)
+	//value, error := reader.ReadString("\n")
+	value, _ := reader.ReadString('\n') // _ blank identifier
+	fmt.Println(value)
+
+	//
+
+	bölüm, kalan := bölme(104, 5)
+	fmt.Println(bölüm, kalan)
+
+}
+
+func merhaba(name string, age int) { // Parameter Function Write
+	fmt.Printf("Adım %s, yaşım %d", name, age)
+}
+
+func result(grade int) string {
+	if grade >= 50 {
+		return "geçtiniz"
+	}
+	return "kaldınız"
+	//fmt.Println("FONKSIYON ICINDEYIM")
+}
+
+// 104 / 5 =====> 20 - 4
+
+func bölme(bölünen, bölen int) (bölüm, kalan int) {
+	bölüm = bölünen / bölen
+	kalan = bölünen % bölen
+
+	return bölüm, kalan
+}
