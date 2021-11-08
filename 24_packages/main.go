8e1c1b67022ecf0b0c56c3599bf24d68e0675ad2
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-11-08 18:37:34 +0300

    packages

---
+++ 24_packages/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	fmt.Print("Lütfen Sınav Sonucunuzu Giriniz: ")
+
+	fmt.Println(strings.Contains("seafood", "foo"))
+
+	fmt.Println(strings.Count("animatrix", "a"))
+
+	fmt.Println(strings.ToUpper("Gopher"))
+
+}
