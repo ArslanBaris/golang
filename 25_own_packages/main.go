d65a3a9f225c3c7239016db0cc7ddcb90eb74c76
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-15 23:38:51 +0300

    getcelcius package added.

--- 25_own_packages/main.go
+++ 25_own_packages/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"getcelcius"
 	"fmt"
+	"golesson/getcelcius"
 )
 
 func main() {
 	fmt.Print("Lütfen Celcius dereceyi giriniz: ")
-	celcius, err := getCelcius.GetCelcius()
-	if err != nil {
-		fmt.Println(err)
-	}
+	celcius, err := getcelcius.GetCelcius()
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 	fahrenheit := (celcius * 9 / 5) + 32
 	fmt.Println(celcius, "Celcius derecesi ", fahrenheit, "Fahrenheit derecesine eşittir.")
 }
