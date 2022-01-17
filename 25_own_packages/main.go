e2238841d8cc696893dd1a2c5e2e68817dd81549
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-17 18:20:05 +0300

    error handling added.

--- 25_own_packages/main.go
+++ 25_own_packages/main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	fmt.Print("Lütfen Celcius dereceyi giriniz: ")
 	celcius, err := getcelcius.GetCelcius()
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
+	if err != nil {
+		fmt.Println(err)
+	}
 	fahrenheit := (celcius * 9 / 5) + 32
 	fmt.Println(celcius, "Celcius derecesi ", fahrenheit, "Fahrenheit derecesine eşittir.")
 }
